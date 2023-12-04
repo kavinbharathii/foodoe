4ac942f52afeb77715b89e92b7fc1c5c2999df8d
kavinbharathii <r.m.kavinbharathi@gmail.com>
2023-12-04 23:36:46 +0530

    mobile resposive: init | font-link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<Head>
+				<link href="https://api.fontshare.com/v2/css?f[]=sharpie@700&f[]=quicksand@700&f[]=satoshi@700,500,400&f[]=array@400&display=swap" rel="stylesheet" />
+			</Head>
 			<title>Foodoe</title>
 			<body className={inter.className}>
 				{children}
